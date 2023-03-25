0e2faaf7a6ccbfdd393400be2eb1fb746b7fa5f3
jaimetamere1 <98675902+jaimetamere1@users.noreply.github.com>
2023-03-25 20:25:03 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+const NOODLES = [
+    {
+        id: 1,
+        name: 'SAMYANG BULDAK NOODLES (original)',
+        type: 'instant fried noodles',
+        description: 'spicy noodles, eat at your own risk',
+        image: 'noodleimages/buldak_spicy_chicken_noodles.jpeg',
+        indepthdescription: "proceed with caution when eating, don't be surprised if it burns a hole through your tongue mercilessly :D. a cup of milk highly recommended if you're trying it for the first time, along with some ice cream so you won't feel the need to drink a whole container of milk in one sitting. the feeling will go away... eventually..",
+        titledescription: 'deliciously spicy noodles with sesame seeds and grilled seaweed flakes',
+        weight: '140g',
+        calories: '530Kcal',
+        expiry: '1 year',
+        release: '04/2012',
+        buylink: 'https://www.amazon.com/Samyang-Chicken-Roasted-Noodles-Flavor/dp/B07H72XJ1K/ref=sr_1_5?crid=FJLML5XPEI1O&keywords=buldak+ramen&qid=1671259613&sprefix=buldak+ram%2Caps%2C315&sr=8-5'
+    },
+
+    {
+        id: 2,
+        name: 'SHIN RAMYUN (orignal)',
+        description: 'mild noodles, proceed with caution if you have low spice tolerance. also kind of flavorless imo',
+        image: 'noodleimages/shinramyun.webp',
+        titledescription: 'a classic, simple ramen that never goes wrong',
+        weight: '120g',
+        calories: '510Kcal',
+        expiry: '8 months',
+        release: '08/1986',
+        indepthdescription: "good for anyone new to instant noodles :) although after eating it a few times and discovering new ramen, it may soon appear like tasteless after a while. it's extremely customizable, and accessible practically anywhere. not recommended if eaten plain - not much flavor.",
+        buylink: 'https://www.amazon.com/NongShim-Ramyun-Noodle-Gourmet-Spicy/dp/B00778B90S/ref=sr_1_1?crid=G3T12A6809KW&keywords=shin%2Bramyun&qid=1671261188&sprefix=shin%2Bramyu%2Caps%2C493&sr=8-1&th=1'
+    },
+
+    {
+        id: 3,
+        name: 'MAMA NOODLES (minced pork flavor)',
+        description: 'not that spicy, like, at all. still an all-time favorite though',
+        image: 'noodleimages/mama-instant-60g-noodles-minced-pork-flavour_700x700.webp',
+        indepthdescription: "an all-time favorite for the inhabitants of southeast asia (notedly cambodia and thailand)! the popular minced pork flavor can bring somewhat of a case of nostalgia to those who grew up eating it and enjoying it.",
+        titledescription: 'deliciously spicy noodles with sesame seeds and grilled seaweed flakes',
+        weight: '60g',
+        calories: '280KCal',
+        expiry: '11 months',
+        release: '04/1972',
+        buylink: 'https://www.amazon.com/Mama-Instant-Noodle-Flavor-Ounce/dp/B00JSVPXVS/ref=sr_1_1?crid=2ODYU3LQFMM6I&keywords=mama+noodles+pork&qid=1671261871&refresh=1&sprefix=mama+noodles%2Caps%2C354&sr=8-1'
+    },
+    
+    {
+        id: 4,
+        name: 'JIN RAMEN (spicy version)',
+        description: 'around the same spice level as shin ramyun, but so much better.',
+        image: 'noodleimages/jinramen.webp',
+        indepthdescription: "literally one of the best korean instant noodles there. no description necessary. shin ramyun noodles but 100x better",
+        titledescription: "a simple burst of flavor every time you try it",
+        weight: '175g',
+        calories: '397KCal',
+        expiry: '1 year',
+        release: '1972',
+        buylink: 'https://www.amazon.com/OTTOGI-Korean-Instant-Tasting-Traditional/dp/B08XN5FRZ2/ref=sr_1_4?crid=2YKLOIBLHVHAT&keywords=jin+ramen&qid=1671272015&sprefix=jin+rame%2Caps%2C512&sr=8-4'
+    },
+
+    {
+        id: 5,
+        name: 'INODMIE MIGORENG (orignal)',
+        description: "good for literally anyone even if they can't eat spicy, one of the best brands out there",
+        image: 'noodleimages/Indomie Mi Goreng Fried Noodles-640x640.png',
+        indepthdescription: "a perfect combination of savoury-ness and spicyness, indomie migoreng will never disappoint. no bad review about it will ever come out of it; it;s good in every way possible.",
+        titledescription: "a simple burst of flavor every time you try it",
+        weight: '175g',
+        calories: '397KCal',
+        expiry: '1 year',
+        release: '1972',
+        buylink: 'https://www.amazon.com/Indomie-Mi-Goreng-Instant-Halal-Noodles-Ounce-Count/dp/B01LYAV5WG/ref=sr_1_1?crid=21IMSLC6ZMZ9Y&keywords=indomie+mi+goreng&qid=1671273343&sprefix=indomie%2Caps%2C512&sr=8-1'
+    }
+]
+
+export default NOODLES
